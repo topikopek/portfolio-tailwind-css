818212eeefdb9e1486fafb4228dc7d9b36e2d455
topikopek <topikcool4@gmail.com>
2026-02-05 23:42:55 +0800

    update dark mode + perbaikan fitur

--- script/script.js
+++ script/script.js
@@ -1,10 +1,16 @@
 // Navbar Fixed
 window.onscroll = function () {
   const header = this.document.querySelector("header");
+  const toTop = document.querySelector("#to-top");
   const fixedNav = header.offsetTop;
+
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
     header.classList.remove("navbar-fixed");
   }
 };
@@ -16,3 +22,31 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme == "dark") {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
